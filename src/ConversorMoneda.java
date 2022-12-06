b7b0dafd102f750ed33505f5ed250d037cb08f38
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-12-05 22:51:20 -0300

    Crea formulario para cargar y mostrar datos
    Se importan librerías necesarias, de java swing y awt.

--- src/ConversorMoneda.java
+++ src/ConversorMoneda.java
@@ -1,3 +1,79 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
 public class ConversorMoneda extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JTextField jtMonedaBase;
+	JTextField jtResultado;
+	
+	public void initialize() {
+		
+		JLabel jpMonedaBase = new JLabel("Ingrese ARS$");
+		jtMonedaBase = new JTextField();
+		
+		JLabel jpResultado = new JLabel("Resultado en USD$");
+		jtResultado = new JTextField();
+		
+		
+		JButton btnConvertir = new JButton("Convertir");
+		btnConvertir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				convertir();
+			}
+		});
+		
+		
+		JPanel jpForm = new JPanel();
+		jpForm.setLayout(new GridLayout(5, 2, 10, 5));
+		
+		jpForm.add(jpMonedaBase);
+		jpForm.add(jtMonedaBase);
+		jpForm.add(jpResultado);
+		jpForm.add(jtResultado);
+		
+		jpForm.add(btnConvertir);
+		
+		JButton btnClear = new JButton("Borrar");
+		
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				clearInputs();
+			}
+		});
+		
+		JPanel jpMainPanel = new JPanel();
+		jpMainPanel.setLayout(new BorderLayout(10, 10));
+		
+		jpMainPanel.add(jpForm, BorderLayout.NORTH);		
+		jpMainPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 20, 50));
+		
+		jpMainPanel.add(btnClear, BorderLayout.SOUTH);
+		
+		add(jpMainPanel);
+		
+		
+		
+		
+		setSize(500, 400);
+		setMinimumSize(new Dimension(450, 400));
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
 }
