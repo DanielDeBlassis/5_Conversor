dae31f0f9a40e6b133bdb578995e554a4f6b96d4
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-12-07 00:50:41 -0300

    Estiliza ventana

--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,16 @@
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 public class Principal {
 	
@@ -21,13 +24,22 @@ public class Principal {
 	    frame.setLocation(500, 100);
 
 	    JPanel panel = new JPanel();
-	    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); // added code
+	    //panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+	    //panel.setLayout(new GridLayout(4, 4, 10, 10));
+	    panel.setLayout(new FlowLayout());
+	    panel.setBackground(new java.awt.Color(175, 178, 203));
+	    panel.setBorder(new LineBorder(Color.white));
 
 	    frame.add(panel);
 
 	    JLabel lbl = new JLabel("Selecciona conversión a realizar y presiona el botón");
 	    lbl.setAlignmentX(Component.CENTER_ALIGNMENT);
+	    //lbl.setForeground(Color.white);
 	    //lbl.setVisible(true); // Not needed
+	    
+	    
+	    panel.setBorder(BorderFactory.createTitledBorder("Seleccione Conversor"));
+	    panel.getRootPane().setBorder(BorderFactory.createMatteBorder(30, 24, 24, 24, new java.awt.Color(227, 229, 244)));
 
 	    panel.add(lbl);
 
